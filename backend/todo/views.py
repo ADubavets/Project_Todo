dfe9981a6eaa486105538c0abee45d47a59c8fd2
ADubavets <d.andrey.f@gmail.com>
2024-10-28 08:44:41 +0300

    28-10-2024/1

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -3,7 +3,7 @@ from .models import Todo
 from .serializers import TodoSerializers
 from .permission import CustomPermission
 from rest_framework import filters, viewsets
-from .pagination import MyLimitOffsetPagination
+# from .pagination import MyLimitOffsetPagination
 
 # Create your views here.
 class TodoViewSet(viewsets.ModelViewSet): 
@@ -12,4 +12,4 @@ class TodoViewSet(viewsets.ModelViewSet):
     permission_classes = [CustomPermission]
     filter_backends = [filters.SearchFilter]
     search_fields = ['title', 'description']
-    pagination_class = MyLimitOffsetPagination
+    # pagination_class = MyLimitOffsetPagination
